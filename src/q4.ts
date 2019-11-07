dc8c4715b46a7c885dc23e55e4d119fbdb9d9bf1
Your Name <you@example.com>
2019-11-07 20:45:40 +0800

    test

--- src/q4.ts
+++ src/q4.ts
@@ -5,11 +5,67 @@
 // 提示: String.includes, String.split
 
 
-let calc = function (calcString: string) {
+//let calc = function (calcString: string) {
+//    for (let i = 1; i < ; i++) {       
+//        for (let j = 1; j < 10; j++) {                    
+//            console.log(`${i} X ${j} = ${i * j}`);    
+//        }   
+//    }
+//}
+//calc('8*2')
+//calc('1+1')
+//calc('5/5')
+//calc('9-1')
 
+let a = function (a: number, b: number) {   
+    for (let i = 1; i <= a; i++) {       
+        for (let j = 1; j <= b; j++) {           
+            console.log(i + ' X ' + j + ' = ' + i * j);           
+            // console.log(`${i} X ${j} = ${i * j}`); // ES6 語法       
+        }   
+    }   
 }
 
-calc('8*2')
-calc('1+1')
-calc('5/5')
-calc('9-1')
+a(8,2)
+//a('1+1')
+//a('5/5')
+//a('9-1')
+
+let c = function (a: number, b: number) {   
+    for (let i = 1; i <= a; i++) {       
+        for (let j = 1; j <= b; j++) {                    
+            console.log(`${i} + ${j} = ${i + j}`); // ES6 語法       
+        }   
+    }   
+}
+
+//a(8,2)
+c(1,1)
+//a('5/5')
+//a('9-1')
+
+let d =function (a: number, b: number) {   
+    for (let i = 1; i <= a; i++) {       
+        for (let j = 1; j <= b; j++) {                    
+            console.log(`${i} / ${j} = ${i / j}`); // ES6 語法       
+        }   
+    }   
+}
+
+//a(8,2)
+//c(1,1)
+d(5,5)
+//a('9-1')
+
+let e =function (a: number, b: number)  {   
+    for (let i = 1; i <= a; i++) {       
+        for (let j = 1; j <= b; j++) {                    
+            console.log(`${i} - ${j} = ${i - j}`); // ES6 語法       
+        }   
+    }   
+}
+
+//a(8,2)
+//c(1,1)
+//d(5,5)
+e(9,1)
